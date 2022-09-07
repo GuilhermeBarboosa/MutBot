df83655f886aa4d4f88f888a1dd00e2d8135d6e3
Guilherme Barbosa <guilherme.strg@gmail.com>
2022-09-07 01:18:45 -0300

    feat: AUTENTICACAO DE CONTA

--- app/src/main/java/treinamento/com/mutbot/LoginActivity.kt
+++ app/src/main/java/treinamento/com/mutbot/LoginActivity.kt
@@ -1,11 +1,71 @@
 package treinamento.com.mutbot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.password
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import treinamento.com.mutbot.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding;
+    private lateinit var googleSignInCliente: GoogleSignInClient
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(layoutInflater);
+        val view = binding.root;
+
+        setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.botaoEntrar.setOnClickListener {
+
+            mAuth!!.createUserWithEmailAndPassword(
+                binding.editTextTextUsuario.toString(),
+                binding.editTextTextSenha.toString()
+            )
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = mAuth!!.currentUser
+
+                        Toast.makeText(
+                            baseContext, "Autenticação efetuada",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Erro de autenticação",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
+
+                    // ...
+                }
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // val currentUser = mAuth!!.currentUser
+        //updateUI(currentUser)
     }
 }
